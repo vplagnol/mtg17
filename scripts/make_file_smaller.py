45724ae28a08973985e3da8fbae6755d40efb83b
Vincent Plagnol <vincent.plagnol@gmail.com>
2025-06-15 15:16:29 +0100

    Initial commit of project setup

---
+++ scripts/make_file_smaller.py
@@ -0,0 +1,29 @@
+## remove information that is not related to "drawn"
+
+import csv
+
+iFile="data/game_data_public.TDM.PremierDraft.csv"
+oFile="processed_data/game_data_public.TDM.PremierDraft_small.csv"
+
+target_strings = {"opening", "deck", "sideboard", "tutored"}
+
+file=open(oFile, 'w', newline='', encoding='utf-8')
+writer = csv.writer(file)
+
+# Read CSV file
+with open(iFile) as fp:
+    csv_reader = csv.reader(fp, delimiter=",", quotechar='"')
+    # Read the header row first (optional)
+    try:
+        header = next(csv_reader)
+        non_matched_index = [
+            index
+            for index, item in enumerate(header) if all(target_str not in item for target_str in target_strings)
+        ]
+        writer.writerow([header[index] for index in non_matched_index])
+    except StopIteration:
+        print("CSV file is empty.")
+        
+    for row in csv_reader:
+        # Each 'row' is a list of strings
+        writer.writerow( [ row[index] for index in non_matched_index])
