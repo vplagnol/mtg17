2e081cb50d6c03109a4c0ec2a8f83a67f5a3da0f
Vincent Plagnol <vincent.plagnol@gmail.com>
2025-06-16 12:26:53 +0100

    Fixed reset of table

--- scripts/combine_all_pair_analyses.R
+++ scripts/combine_all_pair_analyses.R
@@ -7,10 +7,15 @@ for (file in all_files) results[[ file ]] <- readr::read_csv(paste0("temp/", fil
 combined <- dplyr::bind_rows(results)
 
 basic_lands <- c("Forest", "Swamp", "Plains", "Mountain", "Island")
+nonbasic_lands <- c("Bloodfell Caves", "Blossoming Sands", "Swiftwater Cliffs", "Scoured Barrens", "Tranquil Cove")
+exclude_lands <- c(basic_lands, nonbasic_lands)
 
 clean_results <- dplyr::filter(combined, count > 400, interaction_pval < 1e-4) %>% dplyr::arrange(desc(interaction_term))
-clean_results <- dplyr::filter(clean_results, ! card1 %in% basic_lands, ! card2 %in% basic_lands)
+clean_results <- dplyr::filter(clean_results, ! card1 %in% exclude_lands, ! card2 %in% exclude_lands)
 
 print(tail(as.data.frame(clean_results), 10))
 
 print(head(as.data.frame(clean_results), 10))
+
+
+print(all_files)
--- scripts/pair_analysis.R
+++ scripts/pair_analysis.R
@@ -6,7 +6,7 @@ card1 = 'Wail of War'; card2 = 'Shocking Sharpshooter'
 card1 = 'Delta Bloodflies'; card2 = 'Snakeskin Veil'
 
 
-all_data = list()
+
 
 all_card_names =  gsub(grepv(names(card_data), pattern = "GIH_"), pattern = "GIH_", replacement = "")
 pairs_matrix <- combn(all_card_names, 2)
@@ -14,55 +14,69 @@ pairs_matrix <- combn(all_card_names, 2)
 
 ## proceed by chunk of 1K
 if (!file.exists("temp"))  dir.create("temp")
+
+
+
 chunk<- 1
-
-oFile <- paste0("temp/pairs_chunk_", chunk, ".csv")
-while (file.exists(oFile)) {
-    chunk = chunk + 1
-    oFile <- paste0("temp/pairs_chunk_", chunk, ".csv")
-}
-message("Processing chunk ", chunk)
-
-
-
-
 start <- 1000*(chunk-1) + 1
-end <- 1000*chunk + 1
+end <- 1000*chunk
 
-for (i in start:end) {
-    print(i)
-    card1 = pairs_matrix[1,i]
-    card2 = pairs_matrix[2,i]
+
+while (end < ncol(pairs_matrix)) {
     
-    both_GIH = card_data[[paste0('GIH_', card1)]] >= 1 & card_data[[paste0('GIH_', card2)]] >= 1
-    count_pairs = sum( both_GIH )
+    oFile <- paste0("temp/pairs_chunk_", chunk, ".csv")
+    while (file.exists(oFile)) {
+        chunk = chunk + 1
+        oFile <- paste0("temp/pairs_chunk_", chunk, ".csv")
+        start <- 1000*(chunk-1) + 1
+        end <- 1000*chunk
+    }
+    message("Processing chunk ", chunk, " from ", start, " to ", end)
 
-    if (count_pairs > 100) {
-        print(card1)
-        print(card2)
-
-
-        mod <- glm(data = card_data, formula = paste0('won ~ `GIH_', card1, '` * `GIH_', card2, '`'), family = binomial)
-        summary(mod)
-        interaction_pval = as.numeric(coef(summary(mod))[4, 'Pr(>|z|)'])
-        interaction_term = as.numeric(coef(summary(mod))[4, 'Estimate'])
+    all_data = list()
+    for (i in start:end) {
+        print(i)
+        card1 = pairs_matrix[1,i]
+        card2 = pairs_matrix[2,i]
         
-        all_data[[ paste(card1, card2, sep = "_") ]] = list(card1 = card1,
-                                                            card2 = card2,
-                                                            count = count_pairs,
-                                                            interaction_term = interaction_term,
-                                                            interaction_pval = interaction_pval)
+        both_GIH = card_data[[paste0('GIH_', card1)]] >= 1 & card_data[[paste0('GIH_', card2)]] >= 1
+        count_pairs = sum( both_GIH )
+        
+        if (count_pairs > 100) {
+            print(card1)
+            print(card2)
+            
+            
+            mod <- glm(data = card_data, formula = paste0('won ~ `GIH_', card1, '` * `GIH_', card2, '`'), family = binomial)
+            summary(mod)
+            interaction_pval = as.numeric(coef(summary(mod))[4, 'Pr(>|z|)'])
+            interaction_term = as.numeric(coef(summary(mod))[4, 'Estimate'])
+            
+            all_data[[ paste(card1, card2, sep = "_") ]] = list(card1 = card1,
+                                                                card2 = card2,
+                                                                count = count_pairs,
+                                                                interaction_term = interaction_term,
+                                                                interaction_pval = interaction_pval)
+            
+            if (interaction_pval < 0.05) print(summary(mod))
+        }
+        if (i %% 100 == 99) {
+            pair_analysis = dplyr::bind_rows(all_data) %>% dplyr::arrange(desc(interaction_term))
+            interesting <- dplyr::filter(pair_analysis, interaction_pval < 0.05, interaction_term > 0)
+            write.csv(interesting, file = 'temp/interesting_paired_analysis_temp.csv')
+        }
 
-        if (interaction_pval < 0.05) print(summary(mod))
     }
-    if (i %% 100 == 99) {
-        pair_analysis = dplyr::bind_rows(all_data) %>% dplyr::arrange(desc(interaction_term))
-        interesting <- dplyr::filter(pair_analysis, interaction_pval < 0.05, interaction_term > 0)
-        write.csv(interesting, file = 'temp/interesting_paired_analysis_temp.csv')
-    }
+
+    
+    ## let's wrap things up
+    pair_analysis = dplyr::bind_rows(all_data) 
+    write.csv(pair_analysis, file = oFile)
+
+    chunk <- chunk + 1
+    start <- 1000*(chunk-1) + 1
+    end <- 1000*chunk
+
 }
 
-
-pair_analysis = dplyr::bind_rows(all_data) 
-write.csv(pair_analysis, file = oFile)
 
